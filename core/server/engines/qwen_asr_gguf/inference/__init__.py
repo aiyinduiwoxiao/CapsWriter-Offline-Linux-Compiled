799d790e3f2f831e33eb674c50253379a52f821d
aaa <aaa@localhost>
2026-07-06 18:12:45 +0800

    fix: llama_context_params struct mismatch causing segfault on b9881
    - Added missing samplers, n_samplers, ctx_other fields to
      llama_context_params in both qwen_asr_gguf and engines/llama
    - Removed broken try-import of llama in __init__.py that corrupted CWD
    - Added CWD restore on failure in both llama.py init() functions

---
+++ core/server/engines/qwen_asr_gguf/inference/__init__.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+from .. import logger
+
+from .asr import QwenASREngine
+from .aligner import QwenForcedAligner
+from .schema import ForcedAlignItem, ForcedAlignResult, DecodeResult, AlignerConfig, ASREngineConfig, TranscribeResult
+from .chinese_itn import chinese_to_num as itn
+from .audio import load_audio
+from . import exporters
